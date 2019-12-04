da0c654d2ec4d9c3444e4a0c363577d1f0c366e6
cubetastic <cubetastic33@gmail.com>
2019-12-04 22:00:07 +0530

    Add solutions for fourth day

---
+++ src/day_4.rs
@@ -0,0 +1,46 @@
+#[aoc_generator(day4)]
+pub fn input_generator(range: &str) -> Vec<usize> {
+    range.split('-').flat_map(|n| n.parse()).collect()
+}
+
+#[aoc(day4, part1)]
+pub fn number_of_passwords(range: &Vec<usize>) -> usize {
+    let mut n = 0;
+    'passwords: for password in range[0]..range[1] {
+        let mut previous_digit = 0;
+        let mut double_existed = false;
+        for digit in password.to_string().as_bytes() {
+            if *digit > previous_digit {
+                previous_digit = *digit;
+            } else if *digit == previous_digit {
+                double_existed = true;
+            } else {
+                continue 'passwords;
+            }
+        }
+        if double_existed {
+            n += 1;
+        }
+    }
+    n
+}
+
+#[aoc(day4, part2)]
+pub fn more_filtered_number_of_passwords(range: &Vec<usize>) -> usize {
+    let mut n = 0;
+    'passwords: for password in range[0]..range[1] {
+        let mut previous_digit = 0;
+        let mut occurrences = [0; 10];
+        for digit in password.to_string().as_bytes() {
+            if *digit < previous_digit {
+                continue 'passwords;
+            }
+            previous_digit = *digit;
+            occurrences[*digit as usize - 48] += 1;
+        }
+        if occurrences.contains(&2) {
+            n += 1;
+        }
+    }
+    n
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,6 @@ extern crate aoc_runner_derive;
 pub mod day_1;
 pub mod day_2;
 pub mod day_3;
+pub mod day_4;
 
 aoc_lib!{ year = 2019 }
