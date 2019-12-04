86231ee00e8b28b5926035807447cfe8bb4cf3f1
cubetastic <cubetastic33@gmail.com>
2019-12-04 22:17:13 +0530

    Change `.as_bytes()` to `.bytes()` in fourth day's solutions

--- src/day_4.rs
+++ src/day_4.rs
@@ -9,10 +9,10 @@ pub fn number_of_passwords(range: &Vec<usize>) -> usize {
     'passwords: for password in range[0]..range[1] {
         let mut previous_digit = 0;
         let mut double_existed = false;
-        for digit in password.to_string().as_bytes() {
-            if *digit > previous_digit {
-                previous_digit = *digit;
-            } else if *digit == previous_digit {
+        for digit in password.to_string().bytes() {
+            if digit > previous_digit {
+                previous_digit = digit;
+            } else if digit == previous_digit {
                 double_existed = true;
             } else {
                 continue 'passwords;
@@ -31,12 +31,12 @@ pub fn more_filtered_number_of_passwords(range: &Vec<usize>) -> usize {
     'passwords: for password in range[0]..range[1] {
         let mut previous_digit = 0;
         let mut occurrences = [0; 10];
-        for digit in password.to_string().as_bytes() {
-            if *digit < previous_digit {
+        for digit in password.to_string().bytes() {
+            if digit < previous_digit {
                 continue 'passwords;
             }
-            previous_digit = *digit;
-            occurrences[*digit as usize - 48] += 1;
+            previous_digit = digit;
+            occurrences[digit as usize - 48] += 1;
         }
         if occurrences.contains(&2) {
             n += 1;
